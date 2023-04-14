7da888f72073d457a790660b5c9968d1080b031e
fareh-yasmine51 <128979083+fareh-yasmine51@users.noreply.github.com>
2023-04-14 16:46:00 +0100

    Add files via upload

---
+++ yasmine/cc.js
@@ -0,0 +1,18 @@
+
+        let imgbx = document.querySelectorAll('.imgbx');
+        let contentbx = document.querySelectorAll('.contentbx');
+        for (let i=0; i<imgbx.length; i++){
+            imgbx[i].addventlistenere('mouseover',function(){
+                for (let i=0; i<contentbx.length; i++){
+                    contentbx[i].className='contentbx';
+                }
+                document.getElementById(this.dataset.id).
+                className ='contentbx active';
+                for (let i=0; i<imgbx.length; i++){
+                    imgbx[i].className= 'imgbx';
+                }
+                this.className='imgbx active';
+
+            })
+        }
+
